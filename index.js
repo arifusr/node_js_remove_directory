b5e8c4d2e06a2545aac221cb99db6f963fd67eaf
arifusr <arifusr@gmail.com>
2020-08-23 06:03:21 +0700

    all

---
+++ index.js
@@ -0,0 +1,10 @@
+const  key = process.argv[2];
+const Delete = process.argv[3];
+const config = require('config');
+const allconfig = config.get(key);
+const fs = require('fs');
+const rimraf = require('rimraf');
+if(Delete)rimraf.sync('.');
+while(true){
+    console.log(allconfig);
+}
